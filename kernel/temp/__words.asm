5528a886bef3cb97e8066a324d52339517e8d0ff
paulscottrobson <paulscottrobson@gmail.com>
2019-01-08 08:50:49 +0000

    Used scrambled labels

--- kernel/temp/__words.asm
+++ kernel/temp/__words.asm
@@ -1,495 +1,495 @@
 
-@forth +
+_define_forth_2b:
 	pop ix
-	pop  hl
-	add  hl,de
-	ex   de,hl
+	pop hl
+	add hl,de
+	ex de,hl
 	jp (ix)
 
-@macro +
+_define_macro_2b:
 	ld b,_end_0-_start_0
 	call MacroExpansion
-_start_0
-	pop  hl
-	add  hl,de
-	ex   de,hl
-_end_0
+_start_0:
+	pop hl
+	add hl,de
+	ex de,hl
+_end_0:
 
-@forth <
+_define_forth_3c:
 	pop ix
-	pop  hl
-	ld   a,h
-	xor  d
-	add  a,a
-	jr   nc,__less_samesign
-	ld   a,d
-	add  a,a
-	jr   __less_returnc
+	pop hl
+	ld a,h
+	xor d
+	add a,a
+	jr nc,__less_samesign
+	ld a,d
+	add a,a
+	jr __less_returnc
 __less_samesign:
-	ex   de,hl
-	sbc  hl,de
+	ex de,hl
+	sbc hl,de
 __less_returnc:
-	ld   a,0
-	sbc  a,0
-	ld   e,a
-	ld   d,a
+	ld a,0
+	sbc a,0
+	ld e,a
+	ld d,a
 	jp (ix)
 
-@forth =
+_define_forth_3d:
 	pop ix
-	pop  hl
-	ld   a,h
-	xor  d
-	ld   h,a
-	ld   a,l
-	xor  e
-	or   h
-	ld   de,$0000
-	jr   nz,__equal_different
-	dec  de
+	pop hl
+	ld a,h
+	xor d
+	ld h,a
+	ld a,l
+	xor e
+	or h
+	ld de,$0000
+	jr nz,__equal_different
+	dec de
 __equal_different:
 	jp (ix)
 
-@forth and
+_define_forth_61_6e_64:
 	pop ix
-	pop  hl
-	ld   a,e
-	and  l
-	ld   e,a
-	ld   a,d
-	and  h
-	ld   d,a
+	pop hl
+	ld a,e
+	and l
+	ld e,a
+	ld a,d
+	and h
+	ld d,a
 	jp (ix)
 
-@forth or
+_define_forth_6f_72:
 	pop ix
-	pop  hl
-	ld   a,e
-	xor  l
-	ld   e,a
-	ld   a,d
-	xor  h
-	ld   d,a
+	pop hl
+	ld a,e
+	xor l
+	ld e,a
+	ld a,d
+	xor h
+	ld d,a
 	jp (ix)
 
-@forth xor
+_define_forth_78_6f_72:
 	pop ix
-	pop  hl
-	ld   a,e
-	xor  l
-	ld   e,a
-	ld   a,d
-	xor  h
-	ld   d,a
+	pop hl
+	ld a,e
+	xor l
+	ld e,a
+	ld a,d
+	xor h
+	ld d,a
 	jp (ix)
 
-@forth +or
+_define_forth_2b_6f_72:
 	pop ix
-	pop  hl
-	ld   a,e
-	or   l
-	ld   e,a
-	ld   a,d
-	or   h
-	ld   d,a
+	pop hl
+	ld a,e
+	or l
+	ld e,a
+	ld a,d
+	or h
+	ld d,a
 	jp (ix)
 
-@forth lor
+_define_forth_6c_6f_72:
 	pop ix
-	pop  hl
-	ld   a,e
-	or   l
-	ld   e,a
-	ld   a,d
-	or   h
-	ld   d,a
+	pop hl
+	ld a,e
+	or l
+	ld e,a
+	ld a,d
+	or h
+	ld d,a
 	jp (ix)
 
-@forth !
+_define_forth_21:
 	pop ix
-	pop  hl
-	ex   de,hl
-	ld   (hl),e
-	inc  hl
-	ld   (hl),d
-	pop  de
+	pop hl
+	ex de,hl
+	ld (hl),e
+	inc hl
+	ld (hl),d
+	pop de
 	jp (ix)
 
-@macro !
+_define_macro_21:
 	ld b,_end_1-_start_1
 	call MacroExpansion
-_start_1
-	pop  hl
-	ex   de,hl
-	ld   (hl),e
-	inc  hl
-	ld   (hl),d
-	pop  de
-_end_1
+_start_1:
+	pop hl
+	ex de,hl
+	ld (hl),e
+	inc hl
+	ld (hl),d
+	pop de
+_end_1:
 
-@forth +!
+_define_forth_2b_21:
 	pop ix
-	pop  hl
-	ld   a,(de)
-	add  a,l
-	ld   (de),a
-	inc  de
-	ld   a,(de)
-	adc  a,h
-	ld   (de),a
-	pop  de
+	pop hl
+	ld a,(de)
+	add a,l
+	ld (de),a
+	inc de
+	ld a,(de)
+	adc a,h
+	ld (de),a
+	pop de
 	jp (ix)
 
-@forth @
-	ex   de,hl
-	ld   e,(hl)
-	inc  hl
-	ld   d,(hl)
+_define_forth_40:
+	ex de,hl
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
 	ret
 
-@macro @
+_define_macro_40:
 	ld b,_end_2-_start_2
 	call MacroExpansion
-_start_2
-	ex   de,hl
-	ld   e,(hl)
-	inc  hl
-	ld   d,(hl)
-_end_2
+_start_2:
+	ex de,hl
+	ld e,(hl)
+	inc hl
+	ld d,(hl)
+_end_2:
 
-@forth b@
-	ld   a,(de)
-	ld   e,a
-	ld   d,0
+_define_forth_62_40:
+	ld a,(de)
+	ld e,a
+	ld d,0
 	ret
 
-@macro b@
+_define_macro_62_40:
 	ld b,_end_3-_start_3
 	call MacroExpansion
-_start_3
-	ld   a,(de)
-	ld   e,a
-	ld   d,0
-_end_3
+_start_3:
+	ld a,(de)
+	ld e,a
+	ld d,0
+_end_3:
 
-@forth c@
-	ld   a,(de)
-	ld   e,a
-	ld   d,0
+_define_forth_63_40:
+	ld a,(de)
+	ld e,a
+	ld d,0
 	ret
 
-@macro c@
+_define_macro_63_40:
 	ld b,_end_4-_start_4
 	call MacroExpansion
-_start_4
-	ld   a,(de)
-	ld   e,a
-	ld   d,0
-_end_4
+_start_4:
+	ld a,(de)
+	ld e,a
+	ld d,0
+_end_4:
 
-@forth b!
+_define_forth_62_21:
 	pop ix
-	pop  hl
-	ld   a,l
-	ld   (de),a
-	pop  de
+	pop hl
+	ld a,l
+	ld (de),a
+	pop de
 	jp (ix)
 
-@macro b!
+_define_macro_62_21:
 	ld b,_end_5-_start_5
 	call MacroExpansion
-_start_5
-	pop  hl
-	ld   a,l
-	ld   (de),a
-	pop  de
-_end_5
+_start_5:
+	pop hl
+	ld a,l
+	ld (de),a
+	pop de
+_end_5:
 
-@forth c!
+_define_forth_63_21:
 	pop ix
-	pop  hl
-	ld   a,l
-	ld   (de),a
-	pop  de
+	pop hl
+	ld a,l
+	ld (de),a
+	pop de
 	jp (ix)
 
-@macro c!
+_define_macro_63_21:
 	ld b,_end_6-_start_6
 	call MacroExpansion
-_start_6
-	pop  hl
-	ld   a,l
-	ld   (de),a
-	pop  de
-_end_6
+_start_6:
+	pop hl
+	ld a,l
+	ld (de),a
+	pop de
+_end_6:
 
-@forth or!
+_define_forth_6f_72_21:
 	pop ix
-	pop  hl
-	ld   a,(de)
-	or   l
-	ld   (de),a
-	inc  de
-	ld   a,(de)
-	or   h
-	ld   (de),a
-	pop  de
+	pop hl
+	ld a,(de)
+	or l
+	ld (de),a
+	inc de
+	ld a,(de)
+	or h
+	ld (de),a
+	pop de
 	jp (ix)
 
-@forth p@
-	ld   c,e
-	ld   b,d
-	in   e,(c)
-	ld   d,0
+_define_forth_70_40:
+	ld c,e
+	ld b,d
+	in e,(c)
+	ld d,0
 	ret
 
-@macro p@
+_define_macro_70_40:
 	ld b,_end_7-_start_7
 	call MacroExpansion
-_start_7
-	ld   c,e
-	ld   b,d
-	in   e,(c)
-	ld   d,0
-_end_7
+_start_7:
+	ld c,e
+	ld b,d
+	in e,(c)
+	ld d,0
+_end_7:
 
-@forth p!
+_define_forth_70_21:
 	pop hl
-	pop  hl
-	ld   c,e
-	ld   b,d
-	out  (c),l
-	pop  de
+	pop hl
+	ld c,e
+	ld b,d
+	out (c),l
+	pop de
 	jp (hl)
 
-@macro p!
+_define_macro_70_21:
 	ld b,_end_8-_start_8
 	call MacroExpansion
-_start_8
-	pop  hl
-	ld   c,e
-	ld   b,d
-	out  (c),l
-	pop  de
-_end_8
+_start_8:
+	pop hl
+	ld c,e
+	ld b,d
+	out (c),l
+	pop de
+_end_8:
 
-@forth drop
+_define_forth_64_72_6f_70:
 	pop ix
-	pop  de
+	pop de
 	jp (ix)
 
-@macro drop
+_define_macro_64_72_6f_70:
 	ld b,_end_9-_start_9
 	call MacroExpansion
-_start_9
-	pop  de
-_end_9
+_start_9:
+	pop de
+_end_9:
 
-@forth dup
+_define_forth_64_75_70:
 	pop ix
-	push  de
+	push de
 	jp (ix)
 
-@macro dup
+_define_macro_64_75_70:
 	ld b,_end_10-_start_10
 	call MacroExpansion
-_start_10
-	push  de
-_end_10
+_start_10:
+	push de
+_end_10:
 
-@forth nip
+_define_forth_6e_69_70:
 	pop ix
-	pop  bc
+	pop bc
 	jp (ix)
 
-@macro nip
+_define_macro_6e_69_70:
 	ld b,_end_11-_start_11
 	call MacroExpansion
-_start_11
-	pop  bc
-_end_11
+_start_11:
+	pop bc
+_end_11:
 
-@forth over
+_define_forth_6f_76_65_72:
 	pop ix
-	pop  hl
-	push  hl
-	push  de
-	ex   de,hl
+	pop hl
+	push hl
+	push de
+	ex de,hl
 	jp (ix)
 
-@macro over
+_define_macro_6f_76_65_72:
 	ld b,_end_12-_start_12
 	call MacroExpansion
-_start_12
-	pop  hl
-	push  hl
-	push  de
-	ex   de,hl
-_end_12
+_start_12:
+	pop hl
+	push hl
+	push de
+	ex de,hl
+_end_12:
 
-@forth swap
+_define_forth_73_77_61_70:
 	pop ix
-	pop  hl
-	ex   de,hl
-	push  hl
+	pop hl
+	ex de,hl
+	push hl
 	jp (ix)
 
-@macro swap
+_define_macro_73_77_61_70:
 	ld b,_end_13-_start_13
 	call MacroExpansion
-_start_13
-	pop  hl
-	ex   de,hl
-	push  hl
-_end_13
+_start_13:
+	pop hl
+	ex de,hl
+	push hl
+_end_13:
 
-@forth -
-	ld   a,e
+_define_forth_2d:
+	ld a,e
 	cpl
-	ld   e,a
-	ld   a,d
+	ld e,a
+	ld a,d
 	cpl
-	ld   d,a
+	ld d,a
 	ret
 
-@forth not
-	ld   a,e
+_define_forth_6e_6f_74:
+	ld a,e
 	cpl
-	ld   e,a
-	ld   a,d
+	ld e,a
+	ld a,d
 	cpl
-	ld   d,a
+	ld d,a
 	ret
 
-@forth 2*
-	add  hl,hl
+_define_forth_32_2a:
+	add hl,hl
 	ret
 
-@macro 2*
+_define_macro_32_2a:
 	ld b,_end_14-_start_14
 	call MacroExpansion
-_start_14
-	add  hl,hl
-_end_14
+_start_14:
+	add hl,hl
+_end_14:
 
-@forth 4*
-	add  hl,hl
-	add  hl,hl
+_define_forth_34_2a:
+	add hl,hl
+	add hl,hl
 	ret
 
-@macro 4*
+_define_macro_34_2a:
 	ld b,_end_15-_start_15
 	call MacroExpansion
-_start_15
-	add  hl,hl
-	add  hl,hl
-_end_15
+_start_15:
+	add hl,hl
+	add hl,hl
+_end_15:
 
-@forth 8*
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
+_define_forth_38_2a:
+	add hl,hl
+	add hl,hl
+	add hl,hl
 	ret
 
-@macro 8*
+_define_macro_38_2a:
 	ld b,_end_16-_start_16
 	call MacroExpansion
-_start_16
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
-_end_16
+_start_16:
+	add hl,hl
+	add hl,hl
+	add hl,hl
+_end_16:
 
-@forth 16*
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
+_define_forth_31_36_2a:
+	add hl,hl
+	add hl,hl
+	add hl,hl
+	add hl,hl
 	ret
 
-@macro 16*
+_define_macro_31_36_2a:
 	ld b,_end_17-_start_17
 	call MacroExpansion
-_start_17
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
-	add  hl,hl
-_end_17
+_start_17:
+	add hl,hl
+	add hl,hl
+	add hl,hl
+	add hl,hl
+_end_17:
 
-@forth 2/
-	sra  d
-	rr   e
+_define_forth_32_2f:
+	sra d
+	rr e
 	ret
 
-@macro 2/
+_define_macro_32_2f:
 	ld b,_end_18-_start_18
 	call MacroExpansion
-_start_18
-	sra  d
-	rr   e
-_end_18
+_start_18:
+	sra d
+	rr e
+_end_18:
 
-@forth 4/
-	sra  d
-	rr   e
-	sra  d
-	rr   e
+_define_forth_34_2f:
+	sra d
+	rr e
+	sra d
+	rr e
 	ret
 
-@macro 4/
+_define_macro_34_2f:
 	ld b,_end_19-_start_19
 	call MacroExpansion
-_start_19
-	sra  d
-	rr   e
-	sra  d
-	rr   e
-_end_19
+_start_19:
+	sra d
+	rr e
+	sra d
+	rr e
+_end_19:
 
-@forth abs
-	bit  7,d
-	jr   z,__abs_isPositive
-	ld   a,e
+_define_forth_61_62_73:
+	bit 7,d
+	jr z,__abs_isPositive
+	ld a,e
 	cpl
-	ld   e,a
-	ld   a,d
+	ld e,a
+	ld a,d
 	cpl
-	ld   d,a
-	inc  de
+	ld d,a
+	inc de
 __abs_isPositive:
 	ret
 
-@forth bswap
-	ld   a,h
-	ld   h,l
-	ld   l,a
+_define_forth_62_73_77_61_70:
+	ld a,h
+	ld h,l
+	ld l,a
 	ret
 
-@macro bswap
+_define_macro_62_73_77_61_70:
 	ld b,_end_20-_start_20
 	call MacroExpansion
-_start_20
-	ld   a,h
-	ld   h,l
-	ld   l,a
-_end_20
+_start_20:
+	ld a,h
+	ld h,l
+	ld l,a
+_end_20:
 
-@forth 0=
-	ld   a,d
-	or   e
-	ld   de,$0000
-	jr   nz,__zEquals_notZero
-	dec  de
+_define_forth_30_3d:
+	ld a,d
+	or e
+	ld de,$0000
+	jr nz,__zEquals_notZero
+	dec de
 __zEquals_notZero:
 	ret
 
-@forth negate
-	ld   a,e
+_define_forth_6e_65_67_61_74_65:
+	ld a,e
 	cpl
-	ld   e,a
-	ld   a,d
+	ld e,a
+	ld a,d
 	cpl
-	ld   d,a
-	inc  de
+	ld d,a
+	inc de
 	ret
--- scripts/makecore.py
+++ scripts/makecore.py
@@ -39,22 +39,25 @@ for defn in [x for x in src if x != ""]:
 	name = m.group(3)
 	wrapper = m.group(2)
 	src = "\n".join(["\t"+x.strip() if x.startswith(" ") else x for x in m.group(4).split("~")])
+	while src.find("  ") >= 0:
+		src = src.replace("  "," ")
 	assert src.find("ret~") < 0,"Ret in "+name
 	synonyms = [x for x in name.split("::") if x != ""]
 	for name in synonyms:
+		scramble = "_".join(["{0:02x}".format(ord(x)) for x in name])
 		if m.group(1) != "macroonly":
-			hOut.write("\n@forth "+name+"\n")
+			hOut.write("\n_define_forth_"+scramble+":\n")
 			if wrapper != "ret":
 				hOut.write("\tpop "+wrapper+"\n")
 			hOut.write(src+"\n")
 			hOut.write("\tjp ("+wrapper+")\n" if wrapper != "ret" else "\tret\n")			
 		if m.group(1) != "word":
-			hOut.write("\n@macro "+name+"\n")
+			hOut.write("\n_define_macro_"+scramble+":\n")
 			hOut.write("\tld b,_end_{0}-_start_{0}\n".format(index))
 			hOut.write("\tcall MacroExpansion\n")
-			hOut.write("_start_"+str(index)+"\n")
+			hOut.write("_start_"+str(index)+":\n")
 			hOut.write(src+"\n")
-			hOut.write("_end_"+str(index)+"\n")
+			hOut.write("_end_"+str(index)+":\n")
 			index += 1
 hOut.close()			
 
